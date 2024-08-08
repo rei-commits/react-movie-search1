d982385e7c4e5a2d6fd136e754ec5254dd2eb8ec
Andreina <125166401+rei-commits@users.noreply.github.com>
2024-08-08 13:53:22 -0400

    react application updated from codesandbox

---
+++ react-movie/src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
